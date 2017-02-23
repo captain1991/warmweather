f2c7d198fbfdd2f610a9141bb942941ceb23c65a
captain1991 <m18255031805_1@163.com>
2017-02-23 14:07:29 +0800

    添加Httpcallback

--- WarmWeather/app/src/main/java/com/xiaodong/warmweather/util/HttpCallBack.java
+++ WarmWeather/app/src/main/java/com/xiaodong/warmweather/util/HttpCallBack.java
@@ -1,7 +1,22 @@
 package com.xiaodong.warmweather.util;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 /**
  * Created by yxd on 2017/2/23.
  */
-public class HttpCallBack {
+public class HttpCallBack implements Callback {
+    @Override
+    public void onFailure(Call call, IOException e) {
+        LogUtil.e("onFailure======"+e.toString());
+    }
+
+    @Override
+    public void onResponse(Call call, Response response) throws IOException {
+        LogUtil.e("onResponse======="+response.body().string());
+    }
 }
